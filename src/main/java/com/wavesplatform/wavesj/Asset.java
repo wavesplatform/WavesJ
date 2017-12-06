dee3404ecc6068e5d09219deb69c4e6522c14bc1
peterz <peterz@rambler.ru>
2017-12-06 12:32:51 +0300

    Added Asset.WAVES constant to represent WAVES token

---
+++ src/main/java/com/wavesplatform/wavesj/Asset.java
@@ -0,0 +1,8 @@
+package com.wavesplatform.wavesj;
+
+public abstract class Asset {
+    /**
+     * Constant used to represent WAVES token in asset transactions.
+     */
+    public static final String WAVES = "";
+}
