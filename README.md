73775a86f7fd304e835c52a776d2b1400baa4e89
peterz <peterz@rambler.ru>
2018-05-31 16:18:47 +0300

    Version 0.7

--- README.md
+++ README.md
@@ -11,18 +11,18 @@ Use the codes below to add WavesJ as a dependency for your project.
 <dependency>
     <groupId>com.wavesplatform</groupId>
     <artifactId>wavesj</artifactId>
-    <version>0.6</version>
+    <version>0.7</version>
 </dependency>
 ```
 
 ##### Gradle:
 ```
-compile group: 'com.wavesplatform', name: 'wavesj', version: '0.6'
+compile group: 'com.wavesplatform', name: 'wavesj', version: '0.7'
 ```
 
 ##### SBT:
 ```
-libraryDependencies += "com.wavesplatform" % "wavesj" % "0.6"
+libraryDependencies += "com.wavesplatform" % "wavesj" % "0.7"
 ```
 
 [This library's page at Maven Central](https://mvnrepository.com/artifact/com.wavesplatform/wavesj)
--- src/main/java/com/wavesplatform/wavesj/Base64.java
+++ src/main/java/com/wavesplatform/wavesj/Base64.java
@@ -1,17 +1,9 @@
 package com.wavesplatform.wavesj;
 
-//import sun.misc.BASE64Decoder;
-//import sun.misc.BASE64Encoder;
-//
-//import java.io.IOException;
-
 public class Base64 {
-//    private static BASE64Encoder encoder = new BASE64Encoder();
-//    private static BASE64Decoder decoder = new BASE64Decoder();
     private static org.apache.commons.codec.binary.Base64 codec = new org.apache.commons.codec.binary.Base64();
 
     public static String encode(byte[] input) {
-//        return "base64:" + encoder.encode(input);
         return "base64:" + new String(codec.encode(input));
     }
 
@@ -20,11 +12,6 @@ public class Base64 {
             throw new IllegalArgumentException("String of the form base64:chars expected");
         } else {
             return codec.decode(input.substring(7));
-//            try {
-//                return decoder.decodeBuffer(input.substring(7));
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
         }
     }
 }
