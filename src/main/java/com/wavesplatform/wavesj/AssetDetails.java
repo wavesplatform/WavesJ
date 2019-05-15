3b058a98a642ed9ded38c01c7714b9299bff0e41
TheWavesGuy <TheWavesGuy@users.noreply.github.com>
2019-05-15 11:09:55 +0200

    added getter, toString() and hashCode() methods

--- src/main/java/com/wavesplatform/wavesj/AssetDetails.java
+++ src/main/java/com/wavesplatform/wavesj/AssetDetails.java
@@ -52,6 +52,99 @@ public class AssetDetails {
         this.issueTimestamp = issueTimestamp;
     }
 
+    public String getAssetId() {
+        return assetId;
+    }
+
+    public Long getIssueHeight() {
+        return issueHeight;
+    }
+
+    public Long getIssueTimestamp() {
+        return issueTimestamp;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getDecimals() {
+        return decimals;
+    }
+
+    public Boolean getReissuable() {
+        return reissuable;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public Boolean getScripted() {
+        return scripted;
+    }
+
+    public Long getMinSponsoredAssetFee() {
+        return minSponsoredAssetFee;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof AssetDetails))
+            return false;
+
+        final AssetDetails that = (AssetDetails) o;
+
+        if (getAssetId() != null ? !getAssetId().equals(that.getAssetId()) : that.getAssetId() != null)
+            return false;
+        if (getIssueHeight() != null ? !getIssueHeight().equals(that.getIssueHeight()) : that.getIssueHeight() != null)
+            return false;
+        if (getIssueTimestamp() != null ? !getIssueTimestamp().equals(that.getIssueTimestamp()) : that.getIssueTimestamp() != null)
+            return false;
+        if (getIssuer() != null ? !getIssuer().equals(that.getIssuer()) : that.getIssuer() != null)
+            return false;
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null)
+            return false;
+        if (getDescription() != null ? !getDescription().equals(that.getDescription()) : that.getDescription() != null)
+            return false;
+        if (getDecimals() != null ? !getDecimals().equals(that.getDecimals()) : that.getDecimals() != null)
+            return false;
+        if (getReissuable() != null ? !getReissuable().equals(that.getReissuable()) : that.getReissuable() != null)
+            return false;
+        if (getQuantity() != null ? !getQuantity().equals(that.getQuantity()) : that.getQuantity() != null)
+            return false;
+        if (getScripted() != null ? !getScripted().equals(that.getScripted()) : that.getScripted() != null)
+            return false;
+        return getMinSponsoredAssetFee().equals(that.getMinSponsoredAssetFee());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getAssetId() != null ? getAssetId().hashCode() : 0;
+        result = 31 * result + (getIssueHeight() != null ? getIssueHeight().hashCode() : 0);
+        result = 31 * result + (getIssueTimestamp() != null ? getIssueTimestamp().hashCode() : 0);
+        result = 31 * result + (getIssuer() != null ? getIssuer().hashCode() : 0);
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+        result = 31 * result + (getDecimals() != null ? getDecimals().hashCode() : 0);
+        result = 31 * result + (getReissuable() != null ? getReissuable().hashCode() : 0);
+        result = 31 * result + (getQuantity() != null ? getQuantity().hashCode() : 0);
+        result = 31 * result + (getScripted() != null ? getScripted().hashCode() : 0);
+        result = 31 * result + getMinSponsoredAssetFee().hashCode();
+        return result;
+    }
+
     @Override
     public String toString() {
         return "AssetDetails{" +
