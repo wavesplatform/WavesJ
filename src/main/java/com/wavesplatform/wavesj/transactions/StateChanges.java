d2c28c28250c147995eee177f409a63eb6efcd89
amurashko <a.murashko@tradisys.com>
2019-08-29 17:03:34 +0300

    refactoring - empty list is returned instead of null to improve framework usability

--- src/main/java/com/wavesplatform/wavesj/transactions/StateChanges.java
+++ src/main/java/com/wavesplatform/wavesj/transactions/StateChanges.java
@@ -6,6 +6,7 @@ import com.wavesplatform.wavesj.DataEntry;
 import com.wavesplatform.wavesj.Transfer;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class StateChanges {
     private Collection<DataEntry<?>> data;
@@ -14,14 +15,23 @@ public class StateChanges {
     @JsonCreator
     StateChanges(@JsonProperty("data") Collection<DataEntry<?>> data,
                  @JsonProperty("transfers") Collection<OutTransfer> transfers) {
-        this.data = data;
-        this.transfers = transfers;
+        this.data = data != null ? data : Collections.<DataEntry<?>>emptyList();
+        this.transfers = transfers != null ? transfers : Collections.<OutTransfer>emptyList();
     }
 
+    /**
+     * Returns collection of account's {@link DataEntry DataEntries} which were modified after applying transaction
+     * @return collection of {@link DataEntry DataEntries} or empty list
+     */
     public Collection<DataEntry<?>> getData() {
         return data;
     }
 
+    /**
+     * Returns collection of output transfers which were triggered after applying transaction. Currently such transfers
+     * are supported by ScriptInvocationTransaction only
+     * @return collection of transfers or empty list
+     */
     public Collection<OutTransfer> getTransfers() {
         return transfers;
     }
