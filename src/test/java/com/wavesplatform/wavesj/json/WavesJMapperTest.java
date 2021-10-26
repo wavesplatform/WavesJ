0ad81c6cf6c7b44e71fd86391c5d6fe3918b46c5
Michaël van de Giessen <git@tubby.nl>
2021-10-26 16:43:35 +0200

    #69 experimenting with Deserialising json build from waves transactions library

--- src/test/java/com/wavesplatform/wavesj/json/WavesJMapperTest.java
+++ src/test/java/com/wavesplatform/wavesj/json/WavesJMapperTest.java
@@ -1,12 +1,24 @@
 package com.wavesplatform.wavesj.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wavesplatform.crypto.Crypto;
+import com.wavesplatform.transactions.DataTransaction;
 import com.wavesplatform.transactions.Transaction;
+import com.wavesplatform.transactions.account.PrivateKey;
+import com.wavesplatform.transactions.common.Amount;
 import com.wavesplatform.transactions.common.AssetId;
+import com.wavesplatform.transactions.data.DataEntry;
+import com.wavesplatform.transactions.data.StringEntry;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +48,29 @@ class WavesJMapperTest {
         assertEquals(12,transaction.type());
         assertEquals(1,transaction.version());
         assertEquals(1635245671683l,transaction.timestamp());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 10,50,100 })
+    void testDeserializeDataTransactionFromBuilder(int dataCount) throws JsonProcessingException {
+        // setup
+        PrivateKey privateKey = PrivateKey.fromSeed(Crypto.getRandomSeedBytes());
+        ObjectMapper sut = new WavesJMapper();
+
+        List<DataEntry> data = new ArrayList<>();
+        for(int i=0;i<dataCount;i++) {
+            StringEntry.as("test-"+i, UUID.randomUUID().toString());
+        }
+        String json = DataTransaction.builder(data)
+                .fee(Amount.of(100000l))
+                .getSignedWith(privateKey)
+                .toJson();
+
+        // sut
+        Transaction transaction = sut.readValue(json, Transaction.class);
+
+        // verify
+        assertNotNull(transaction);
+        assertEquals(privateKey.address(),transaction.sender().address());
     }
 }
